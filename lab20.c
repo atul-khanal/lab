ab27c247c70c40b18a3e9bc6a6a9d4b3f7bff9c1
atul-khanal <83754434+atul-khanal@users.noreply.github.com>
2021-07-08 13:59:56 +0545

    gitignore

--- lab20.c
+++ lab20.c
@@ -1,21 +1,22 @@
 //write a c program that reads a number and finds its reverse using function
 #include<stdio.h>
+int reverse(int);
 int main()
 {
     int n;
     printf("enter any number : ");
     scanf("%d",&n);
-    print("the reverse of %d",n,reverse(n));
-
+    printf("the reverse of %d is %d",n,reverse(n));
+    return 0;
 }
-int reverese(int n)
+int reverse(int n)
 {
     int d,r=0;
     while(n>0)
     {
         d=n%10;
         r=r*10+d;
-        n=n/10
+        n=n/10;
     }
     return r;
 }
