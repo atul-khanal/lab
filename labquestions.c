6b26e017ea231b8fc28413c721d682d945b55d81
atul-khanal <83754434+atul-khanal@users.noreply.github.com>
2021-07-14 14:13:06 +0545

    lab questions

---
+++ labquestions.c
@@ -0,0 +1,165 @@
+/* 
+
+Lab 1 
+
+2021/05/13 Thursday 
+
+Write a program that reads a number and tests whether it is multiple of 7 or not.*/ 
+
+ 
+
+/* 
+
+Lab 2 
+
+Write a C program that reads marks in 5 subjects of a student and then test whether the sutdent is fail or pass 
+
+2021/05/13 
+
+*/ 
+
+ 
+
+/* 
+
+Lab 3 
+
+Write a c program that reads two digit number and find its sum of digits  (For example if number is 76 then its sum is 13) 
+
+2021/05/13 
+
+*/ 
+
+ 
+
+//Lab 4 
+
+//Write a c program that reads a number and tests whether it is prime or composite. 
+
+ 
+
+//Lab 5 
+
+//Write a c program that reads a number and finds sum of its digits 
+
+ 
+
+ 
+
+//Lab 6 
+
+//Write a program that reads a number and finds its reverse(123-->321) 
+
+//2021/05/21 
+
+ 
+
+//Lab 7 
+
+//Write a C program that reads a number and tests whether it is palindorme or not 
+
+ 
+
+//Lab 8 
+
+//Write a C program that reads a number and finds sum of cubes of digit 
+
+ 
+
+//Lab:9 
+
+//Write a C program that prints primes from 200 to 500 
+
+ 
+
+//Lab 10 
+
+//2021/05/27 
+
+//Write a c program that reads cofficent of quadritc equation and finds its root 
+
+ 
+
+//Lab 11(2021/06/03) 
+
+//write a c program that reads 10 numbers and sort them in ascending order 
+
+//sorting: It is the technique of arranging data in ascending or decending in order. 
+
+ 
+
+//La 12 
+
+//Write a c program that searches key element in an array of size 10 
+
+ 
+
+ 
+
+//Lab 13 
+
+//Write a c program that checks if the array contains duplicate element. 
+
+ 
+
+//Lab 14 (2021/06/04) 
+
+//Wrire a c program that reads given 10 numbers and finds sum of square of each number 
+
+ 
+
+//Lab 15 
+
+//Write a c program that reads 10 numbers an finds frequency of each elements 
+
+ 
+
+//ab 16 2021/06/10 
+
+//Write a c program to find the sum, difference and product of two 3*3 matrices. 
+
+ 
+
+//Lab 17: 
+
+//Write a C program that reads a word and checks whether it is palindrome or not 
+
+ 
+
+//Lab 18: 
+
+//Write a C program that prints first 30 fibonacci numbers using user defined function 
+
+ 
+
+//Lab 19: 
+
+//Write a c program that prints prime numbers from 200 to 300 using function 
+
+ 
+
+//Lab 20: 
+
+//Write a c program that reads a number and find its reverse using function 
+
+ 
+
+ 
+
+//Lab 21		2021/07/08 
+
+//Create a structure time(hour, minute, second) then create two time variables t1(1,40,30) and t2(2,30,50) and then them bys stroring sum on time t3 
+
+ 
+
+ 
+
+//Lab 22: 
+
+//Ma sanga xaina raxa que,….. :’( 
+
+  
+
+// Lab23 : Write a C program that reads rollno, name and marks of 10 students 
+
+//and then display the details of those students whose marks is greater than 80 
